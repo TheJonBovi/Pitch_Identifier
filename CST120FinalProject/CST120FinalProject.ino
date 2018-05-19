7ec0b8636a0d69edad6d01efa08c9f622002d176
an6688 <anil.coklar@oit.edu>
2018-05-18 21:48:36 -0700

    first update
    not much done :herb:

--- CST120FinalProject/CST120FinalProject.ino
+++ CST120FinalProject/CST120FinalProject.ino
@@ -1,9 +1,90 @@
-void setup() {
-  // put your setup code here, to run once:
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
 
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x32 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()   {                
+  Serial.begin(9600);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  
+  display.display();
+  delay(2000);
+
+  display.clearDisplay();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  testscrolltext();
+  delay(2000);
+  display.clearDisplay();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  
+}
+
+void testscrolltext(void) {
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.clearDisplay();
+  int x = 10; 
+  display.println("Frequency in hz is:", x); 
+  //display.x; 
+  display.display();
+  delay(1);
 }
