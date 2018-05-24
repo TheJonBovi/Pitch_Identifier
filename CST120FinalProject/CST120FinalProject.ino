84e48f7dcfc555da0e331be2596a9c1524578cdc
Drew <35358211+TheJonBovi@users.noreply.github.com>
2018-05-24 15:28:04 -0700

    frequency detection via midpoint detection added
    Real world noise too complex for this method probably.
    Looking into fourier analysis.

--- CST120FinalProject/CST120FinalProject.ino
+++ CST120FinalProject/CST120FinalProject.ino
@@ -54,36 +54,102 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-void setup()   {                
+// Flag for audio input clipping
+boolean clipping = 0;
+
+// Storage to compare ADC values
+byte newRead = 0;
+byte prevRead = 0;
+
+// Frequency calculation variables
+unsigned int timer = 0;
+unsigned int period;
+int frequency;
+
+void setup()   
+{
+  // SETUP DISPLAY          
   Serial.begin(9600);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
-  
+
+  // Display splash screen for 1 second
   display.display();
   delay(2000);
 
-  display.clearDisplay();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  testscrolltext();
-  delay(2000);
-  display.clearDisplay();
-}
+  // Show Frequency Text
+  show_text();
 
 
-void loop() {
+  // SETUP MICROPHONG ADC
+  pinMode(13,OUTPUT);//led indicator pin
+  
+  cli();//disable interrupts
+  
+  //set up continuous sampling of analog pin 0
+  
+  //clear ADCSRA and ADCSRB registers
+  ADCSRA = 0;
+  ADCSRB = 0;
+  
+  ADMUX |= (1 << REFS0); //set reference voltage
+  ADMUX |= (1 << ADLAR); //left align the ADC value- so we can read highest 8 bits from ADCH register only
+  
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //set ADC clock with 32 prescaler- 16mHz/32=500kHz
+  ADCSRA |= (1 << ADATE); //enabble auto trigger
+  ADCSRA |= (1 << ADIE); //enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN); //enable ADC
+  ADCSRA |= (1 << ADSC); //start ADC measurements
+  
+  sei();//enable interrupts
   
 }
 
-void testscrolltext(void) {
+// INTERRUPT SERVICE ROUTINE FOR ADC READ
+ISR(ADC_vect) 
+{
+  prevRead = newRead;//store previous value
+  newRead = ADCH;//get value from A0
+
+  //if increasing and crossing midpoint
+  if (prevRead < 127 && newRead >=127)
+  {
+    period = timer;//get period
+    timer = 0;//reset timer
+  }
+  
+  if (newRead == 0 || newRead == 1023){//if clipping
+    PORTB |= B00100000;//set pin 13 high- turn on clipping indicator led
+    clipping = 1;//currently clipping
+  }
+  
+  timer++;//increment timer at rate of 38.5kHz
+}
+
+void loop() 
+{  
+  if (clipping)
+  {
+    PORTB &= B11011111; //turn off clipping indicator led
+    clipping = 0;
+  }
+
+   frequency = 38462/period;//timer rate/period
+  
+   // Print results
+   show_text();
+
+   delay(100);
+}
+
+void show_text(void) {
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(10,0);
   display.clearDisplay();
-  int x = 10; 
-  display.println("Frequency in hz is:", x); 
+  display.println(frequency);
+  //int x = 10; 
+  //display.println("Frequency in hz is:"); 
   //display.x; 
   display.display();
   delay(1);
